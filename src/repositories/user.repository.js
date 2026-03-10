b5ffd5fa5707b3b37e82ea57e5d24fd9ee572b68
Code Source <codesource2001@gmail.com>
2026-03-10 21:57:54 +0530

    findByEmail method implemented

--- src/repositories/user.repository.js
+++ src/repositories/user.repository.js
@@ -4,6 +4,16 @@ const crudRepository = require("../repositories/crud.repository")
 
 const userRepository = {
     ...crudRepository(User),
+    
+    findByEmail: async (email) => {
+        try {
+            const user = await User.findOne({ email: email })
+            return user;
+        } catch (error) {
+            throw error;
+        }
+    }
+
 }
 
 module.exports = userRepository;
